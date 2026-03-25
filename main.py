0a66d81059e4a0f880146c3e1d132698919a5359
Muhamed Esmeal <162034075+ProgrammersCoffee@users.noreply.github.com>
2026-03-26 00:54:03 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,2 @@
 from transformers import pipeline
-
-chatbot = pipeline("text-generation", model="bigscience/bloomz-560m")
-user_message = "what would i cook for dinner "
-response=chatbot(user_message)
-print(response)
+from base import Chatbot ,bot_reply 
